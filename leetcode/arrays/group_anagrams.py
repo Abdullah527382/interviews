4c6459df7461e4dc2d37b5059d4b5ffc95f43616
Abdullah <abdullah527382@gmail.com>
2024-03-15 06:50:18 +1100

    group anagrams together

---
+++ leetcode/arrays/group_anagrams.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hashmap = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            hashmap[tuple(count)].append(s)
+        return hashmap.values()
